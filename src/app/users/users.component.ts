7de9ef881051e84386324e9ad9be055d81034108
Michał Wajer <wajer.michael@gmail.com>
2020-09-24 18:56:47 +0200

    Wykorzystanie danych w szablonie

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.interface';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  public users: User[] = [
+    { id: 1, name: 'Jan', phone: 123321123, email: 'jan@jan.com' },
+    { id: 2, name: 'Maria', phone: 455645564, email: 'maria@maria.com' },
+    { id: 3, name: 'Ola', phone: 78998987, email: 'ola@ola.com' },
+  ];
 
   constructor() { }
 
