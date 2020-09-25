87e8b895e4506f2eff29c7fae6a7843bdf62166e
Michał Wajer <wajer.michael@gmail.com>
2020-09-25 17:24:28 +0200

    Tworzenie głównego routingu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { BigLettersPipe } from './big-letters.pipe';
 import { UserComponent } from './user/user.component';
 import { BgGreenDirective } from './bg-green.directive';
 import { BgMouseoverDirective } from './bg-mouseover.directive';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NewUserFormComponent } from './new-user-form/new-user-form.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'comments', component: CommentsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,11 +29,14 @@ import { NewUserFormComponent } from './new-user-form/new-user-form.component';
     UserComponent,
     BgGreenDirective,
     BgMouseoverDirective,
-    NewUserFormComponent
+    NewUserFormComponent,
+    PostsComponent,
+    CommentsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     HttpClientModule
   ],
